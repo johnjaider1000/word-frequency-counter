96d2e1ed2d738a220f722eeeac96aa417493a8f8
johnjaider1000 <jhonjaider100015@gmail.com>
2025-11-19 19:35:25 -0500

    chore: setup tailwind and organize project structure

---
+++ src/core/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="text-3xl font-bold text-red-500">Welcome!</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
